63f49f1923699a07ba1e6fc84b89c70755f73e7e
James Brand <jamesbrandecon@gmail.com>
2024-06-28 16:27:05 -0700

    new constraint check for SMC testing

--- src/constraint_checks.jl
+++ src/constraint_checks.jl
@@ -15,11 +15,13 @@ function report_constraint_violations(problem;
 
     violations = Dict();
     all_satisfied = ones(Bool, size(problem.data,1));
-    
+    num_violated_per_market = zeros(Int64, size(problem.data,1));
+
     # Monotonicity
     if :monotone in problem.constraints
         monotone_satisfied = [all(diag(reshaped_jacobians[:,:,i]) .<=0) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& monotone_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(monotone_satisfied));
         frac_monotone_violations = round(1 - mean(monotone_satisfied), digits = 2);
         verbose && println("Fraction of violations of :monotonicity: $frac_monotone_violations")
         push!(violations, :monotone => frac_monotone_violations)
@@ -29,6 +31,7 @@ function report_constraint_violations(problem;
     if :all_substitutes in problem.constraints
         all_subs_satisfied = [check_all_subs(reshaped_jacobians[:,:,i]) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& all_subs_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(all_subs_satisfied));
         frac_all_subs_violations = round(1 - mean(all_subs_satisfied), digits = 2);
         verbose && println("Fraction of violations of :all_substitutes: $frac_all_subs_violations")
         push!(violations, :all_substitutes => frac_all_subs_violations)
@@ -38,6 +41,7 @@ function report_constraint_violations(problem;
     if :diagonal_dominance_all in problem.constraints
         diag_dom_satisfied = [check_diagonal_dominance(reshaped_jacobians[:,:,i]) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& diag_dom_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(diag_dom_satisfied));
         frac_diag_dom_violations = round(1 - mean(diag_dom_satisfied), digits = 2);
         verbose && println("Fraction of violations of :diagonal_dominance_all: $frac_diag_dom_violations")
         push!(violations, :diagonal_dominance_all => frac_diag_dom_violations)
@@ -47,6 +51,7 @@ function report_constraint_violations(problem;
     if :subs_in_group in problem.constraints
         subs_in_group_satisfied = [check_subs_in_group(reshaped_jacobians[:,:,i], problem.exchange) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& subs_in_group_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(subs_in_group_satisfied))
         frac_subs_in_group_violations = round(1 - mean(subs_in_group_satisfied), digits = 2);
         verbose && println("Fraction of violations of :subs_in_group: $frac_subs_in_group_violations")
         push!(violations, :subs_in_group => frac_subs_in_group_violations)
@@ -56,6 +61,7 @@ function report_constraint_violations(problem;
     if :subs_across_group in problem.constraints
         subs_across_group_satisfied = [check_subs_across_group(reshaped_jacobians[:,:,i], problem.exchange) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& subs_across_group_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(subs_across_group_satisfied))
         frac_subs_across_group_violations = round(1 - mean(subs_across_group_satisfied), digits = 2);
         verbose && println("Fraction of violations of :subs_across_group: $frac_subs_across_group_violations")
         push!(violations, :subs_across_group => frac_subs_across_group_violations)
@@ -65,6 +71,7 @@ function report_constraint_violations(problem;
     if :all_complements in problem.constraints
         all_comps_satisfied = [check_all_complements(reshaped_jacobians[:,:,i]) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& all_comps_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(all_comps_satisfied))
         frac_all_comps_violations = round(1 - mean(all_comps_satisfied), digits = 2);
         verbose && println("Fraction of violations of :all_complements: $frac_all_comps_violations")
         push!(violations, :all_complements => frac_all_comps_violations)
@@ -74,6 +81,7 @@ function report_constraint_violations(problem;
     if :complements_in_group in problem.constraints
         complements_in_group_satisfied = [check_comps_in_group(reshaped_jacobians[:,:,i], problem.exchange) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& complements_in_group_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(complements_in_group_satisfied))
         frac_complements_in_group_violations = round(1 - mean(complements_in_group_satisfied), digits = 2);
         verbose && println("Fraction of violations of :complements_in_group: $frac_complements_in_group_violations")
         push!(violations, :complements_in_group => frac_complements_in_group_violations)
@@ -83,6 +91,7 @@ function report_constraint_violations(problem;
     if :complements_across_group in problem.constraints
         complements_across_group_satisfied = [check_comps_across_group(reshaped_jacobians[:,:,i], problem.exchange) for i in 1:size(reshaped_jacobians,3)];
         all_satisfied = all_satisfied .& complements_across_group_satisfied;
+        num_violated_per_market = num_violated_per_market .+ (1 .- Int.(complements_across_group_satisfied))
         frac_complements_across_group_violations = round(1 - mean(complements_across_group_satisfied), digits = 2);
         verbose && println("Fraction of violations of :complements_across_group: $frac_complements_across_group_violations")
         push!(violations, :complements_across_group => frac_complements_across_group_violations)
@@ -93,6 +102,8 @@ function report_constraint_violations(problem;
     
     if output == "dict"
         return violations
+    elseif output == "count"
+        num_violated_per_market
     else
         return Float64::violations[:any]
     end
